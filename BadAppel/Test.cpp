4ca900f8202a3c0e1c7bd9448305af0f642a242f
NaztarHex <qhmed0826@gmail.com>
2021-03-02 18:45:43 +0100

    720P IS HERE

--- BadAppel/Test.cpp
+++ BadAppel/Test.cpp
@@ -9,56 +9,56 @@ using namespace std;
 
 
 
-int nScreenWidth = 48;
-int nScreenHeight = 36;
+int nScreenWidth = 960;
+int nScreenHeight = 720;
 int main() {
     wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
     HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
     SetConsoleActiveScreenBuffer(hConsole);
     DWORD dwBytesWritten = 0;
-    string path = "Resources/e.mp4";
+    string path = "Resources/Bad Apple.mp4";
     VideoCapture cap(path);
     Mat img, graymat;
     int Pixel;
-    int Frame=0;
+    int Frame = 0;
     while (cap.isOpened())
     {
-        
+
         cap.read(img);
-        cvtColor(img,graymat, COLOR_BGR2GRAY);
+        cvtColor(img, graymat, COLOR_BGR2GRAY);
 
         int x = 0;
-        for (int j = 0; j < graymat.rows; j++)
+        for (int j = 0; j < graymat.rows; j=j+2)
         {
-            screen[x] = ';';
-            x++;
+
 
             for (int i = 0; i < graymat.cols; i++)
             {
 
-                
-                if (img.at<char>(j, i) == 0) {
+
+                if (img.at<char>(j, i*3) == 0) {
                     screen[x] = ' ';
                 }
-                else 
+                else if ((img.at<char>(j, i * 3) > 0) && (img.at<char>(j, i * 3) < 240)) {
+                    screen[x] = '.';
+                }
+                else
                 {
                     screen[x] = 'a';
                 }
-             
+
                 x++;
-            
+
             }
-                        screen[x] = ';';
-            x++;
+            
+           
 
 
         }
         screen[nScreenWidth * nScreenHeight - 1] = '\0';
         WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
-        imshow("My Image", graymat);
-        waitKey(20);
-       
 
-      
+
+        /*Naztar*/
     }
 }
